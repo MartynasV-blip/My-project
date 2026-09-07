4ef675b55f01b01c2f1570ebeb279a7153a72559
Milkas <edgaras.stat@gmail.com>
2026-09-07 14:41:43 +0300

    Added basic escape menu

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    [SerializeField] private GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
